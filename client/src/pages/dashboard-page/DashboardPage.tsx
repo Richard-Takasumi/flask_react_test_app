42bd47f27a93d8d22ab6aab0a95918e5e6e60958
RichardT <30628564+Richard-Takasumi@users.noreply.github.com>
2023-07-29 17:08:34 +0800

    Line graph example added
    On /dashboard page

--- client/src/pages/dashboard-page/DashboardPage.tsx
+++ client/src/pages/dashboard-page/DashboardPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './DashboardPage.scss'
+import Line  from "../../components/line-graph/LineGraph"
 
 export const DashboardPage = () => {
     return (
         <div>
+            <div>
+                <Line />
+            </div>
             <h1 className='test'>DashboardPage</h1>
         </div>
     )
