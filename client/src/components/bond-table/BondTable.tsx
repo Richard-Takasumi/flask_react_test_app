f3971281b3d07af4ed4f91d5e4d438afd0579148
RichardT <richie.takasumi@gmail.com>
2023-07-31 20:41:04 +0800

    Bond Page update
    Added working table w/ dummy data
    Added Prediction w/ formatting

--- client/src/components/bond-table/BondTable.tsx
+++ client/src/components/bond-table/BondTable.tsx
@@ -1,105 +1,62 @@
-import React, { useState } from 'react';
-import { useTable, useBlockLayout } from 'react-table';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+import './BondTable.scss'
+// Create a dark theme instance
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
-// Define all possible columns
-const allColumns = [
-  { Header: 'Name', accessor: 'name' },
-  { Header: 'Ticker', accessor: 'ticker' },
-  { Header: 'Cpn', accessor: 'cpn' },
-  { Header: 'Maturity', accessor: 'maturity' },
-  { Header: 'Spread', accessor: 'spread' },
-  { Header: 'YTM%', accessor: 'ytm' },
-  { Header: "Moody's Rating", accessor: 'moodysRating' },
-  { Header: 'Cr. Mig Pred', accessor: 'crMigPred' },
-  { Header: 'Cr. Mig CL', accessor: 'crMigCL' },
-  { Header: 'Cr. Spread Pred', accessor: 'crSpreadPred' },
-  { Header: 'Cr. Spread SL', accessor: 'crSpreadSL' },
-  // Add more columns as needed
+const useStyles = makeStyles({
+  rowSelected: {
+    backgroundColor: '#0077E4',
+  },
+});
+
+const columns = [
+  { field: 'name', headerName: 'Name' },
+  { field: 'ticker', headerName: 'Ticker' },
+  { field: 'cpn', headerName: 'Cpn' },
+  { field: 'maturity', headerName: 'Maturity' },
+  { field: 'spread', headerName: 'Spread' },
+  { field: 'ytm', headerName: 'YTM%' },
+  { field: 'moodysRating', headerName: "Moody's Rating" },
+  { field: 'crMigPred', headerName: 'Cr. Mig Pred' },
+  { field: 'crMigCL', headerName: 'Cr. Mig CL' },
+  { field: 'crSpreadPred', headerName: 'Cr. Spread Pred' },
+  { field: 'crSpreadSL', headerName: 'Cr. Spread SL' },
 ];
 
-
-const BondTable = ({ data }) => {
-    const [columns, setColumns] = useState(allColumns.slice(0, 5)); // Initially take the first five columns
-  
-    const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      rows,
-      prepareRow,
-    } = useTable({ columns, data }, useBlockLayout);
-  
-    const handleDragEnd = (result) => {
-      if (!result.destination) return;
-      const { source, destination } = result;
-      const movedColumn = columns[source.index];
-      const remainingColumns = columns.filter((col, index) => index !== source.index);
-      remainingColumns.splice(destination.index, 0, movedColumn);
-      setColumns(remainingColumns);
-    };
-  
-    return (
-      <>
-        {/* UI for adding/removing columns */}
-        <div>
-          {allColumns.map((column, index) => (
-            <div key={index}>
-              <input
-                type="checkbox"
-                checked={columns.includes(column)}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    setColumns(old => [...old, column]);
-                  } else {
-                    setColumns(old => old.filter(col => col !== column));
-                  }
-                }}
-              />
-              {column.Header}
-            </div>
-          ))}
-        </div>
-        {/* The table itself */}
-        <DragDropContext onDragEnd={handleDragEnd}>
-          <Droppable droppableId="table" direction="horizontal">
-            {(provided) => (
-              <div {...provided.droppableProps} ref={provided.innerRef}>
-                <table {...getTableProps()}>
-                  <thead>
-                    {headerGroups.map(headerGroup => (
-                      <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column, index) => (
-                          <Draggable draggableId={column.id} index={index} key={column.id}>
-                            {(provided) => (
-                              <th {...column.getHeaderProps()} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                {column.render('Header')}
-                              </th>
-                            )}
-                          </Draggable>
-                        ))}
-                      </tr>
-                    ))}
-                  </thead>
-                  <tbody {...getTableBodyProps()}>
-                    {rows.map(row => {
-                      prepareRow(row);
-                      return (
-                        <tr {...row.getRowProps()}>
-                          {row.cells.map(cell => (
-                            <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                          ))}
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </>
-    );
+export default function BondTable({ data, onRowSelected }) {
+  const classes = useStyles();
+  const [selectedRow, setSelectedRow] = React.useState(null);
+  const handleRowClick = (param) => {
+    setSelectedRow(param.id);
   };
-  
-  export default BondTable;
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          getRowId={(row) => row.id} // Here I'm using the `name` property as the id. Replace `name` with the appropriate property in your data.
+          onRowSelectionModelChange={onRowSelected}
+          onRowClick={handleRowClick}
+          rowSelectionModel={[]}
+          getRowClassName={(params) =>
+            params.id === selectedRow ? classes.rowSelected : ''
+          }
+          sx={{
+            backgroundColor: "#1D232C",
+            borderRadius: '16px',
+            border: "0px"
+          }}
+        />
+      </div>
+    </ThemeProvider>
+  );
+}
--- client/src/pages/bond-overview-page/BondOverviewPage.tsx
+++ client/src/pages/bond-overview-page/BondOverviewPage.tsx
@@ -4,11 +4,26 @@ import './BondOverviewPage.scss'
 import CountrySelect from '../../components/country-select/CountrySelect'
 import CompanySelect from '../../components/company-select/CompanySelect'
 import BondTable from '../../components/bond-table/BondTable';
+import Button from '../../components/button/Button';
+type DataRow = {
+    id: number;
+    name: string;
+    ticker: string;
+    cpn: string;
+    maturity: string;
+    spread: string;
+    ytm: string;
+    moodysRating: string;
+    crMigPred: string;
+    crMigCL: string;
+    crSpreadPred: string;
+    crSpreadSL: string;
+  };
 
 export const BondOverviewPage = () => {
     const [selectedCountry, setSelectedCountry] = useState<{label: string, value: string}|null>(null);
     const [selectedCompany, setSelectedCompany] = useState<{label: string, value: string}|null>(null);  
-
+    const [selectedRow, setSelectedRow] = useState<DataRow | null>(null);
     const handleCountryChange = (selectedCountry) => {
         setSelectedCountry(selectedCountry);
         console.log('Selected country: ', selectedCountry);
@@ -21,21 +36,64 @@ export const BondOverviewPage = () => {
 
     const data = [
         {
-          name: "Company 1",
-          ticker: "C1",
-          cpn: "Cpn 1",
-          maturity: "Maturity 1",
-          spread: "Spread 1",
-          ytm: "YTM 1",
-          moodysRating: "Rating 1",
-          crMigPred: "Pred 1",
-          crMigCL: "CL 1",
-          crSpreadPred: "Spread Pred 1",
-          crSpreadSL: "Spread SL 1",
+          id: 1,
+          name: "MS1 INVF Global Convertible Bond Fund",
+          ticker: "LU12345678",
+          cpn: "4.15",
+          maturity: "30/10/2025",
+          spread: "6.31",
+          ytm: "8.960%",
+          moodysRating: "Ba",
+          crMigPred: "B",
+          crMigCL: "52%",
+          crSpreadPred: "4.2 (+6%)",
+          crSpreadSL: "88%",
         },
-        // More objects for more rows
+        {
+          id: 2,
+          name: "MS2 INVF Global Convertible Bond Fund",
+          ticker: "LU12345678",
+          cpn: "4.15",
+          maturity: "30/10/2025",
+          spread: "6.31",
+          ytm: "8.960%",
+          moodysRating: "Ba",
+          crMigPred: "B",
+          crMigCL: "52%",
+          crSpreadPred: "4.2 (+6%)",
+          crSpreadSL: "88%",
+        },
+        {
+          id: 3,
+          name: "MS3 INVF Global Convertible Bond Fund",
+          ticker: "LU12345678",
+          cpn: "4.15",
+          maturity: "30/10/2025",
+          spread: "6.31",
+          ytm: "8.960%",
+          moodysRating: "Ba",
+          crMigPred: "B",
+          crMigCL: "52%",
+          crSpreadPred: "4.2 (+6%)",
+          crSpreadSL: "88%",
+        },
+
       ];
-      
+
+    const handleRowSelected = (selectionModel) => {
+    if (selectionModel.length > 0) {
+        const selectedId = selectionModel[0];  // Get the first selected id
+        const selectedRowData = data.find((row) => row.id === selectedId);
+        if (selectedRowData) {
+        setSelectedRow(selectedRowData);
+        console.log('Selected row: ', selectedRowData);
+        } else {
+        setSelectedRow(null);
+        }
+    } else {
+        setSelectedRow(null);
+    }
+    };
 
     return (
         <div className='bond-page-container'>
@@ -48,10 +106,43 @@ export const BondOverviewPage = () => {
                     <CompanySelect onCompanyChange={handleCompanyChange} />
                 </div>
             </div>
-            <BondTable data={data} />
+            <div className='bond-table-container'>
+                <BondTable data={data} onRowSelected={handleRowSelected} />
+            </div>
+            <div className='bond-selected-container'>
+                <div className='bond-selected-item'>
+                    {selectedRow && (<>
+                        <div className='bond-selected-title'>
+                            <div className='bond-selected-title-text'>
+                                <p className='bond-selected-name'>{selectedRow.name}</p>
+                                <p className='bond-selected-ticker'>{selectedRow.ticker}</p>
+                            </div>
+                            <div className='bond-selected-title-watchlist-button'>
+                                <Button text={"Watchlist +"}/>
+                            </div>
+                        </div>
+                        <div className='bond-selected-predictions'>
+                            <div className='bond-selected-credit-migration'>
+                                <h2> Probability of Credit Migration </h2>
+                                <div className='bond-selected-credit-migration-stats'>
+                                    <p className='bond-selected-credit-percentage'>{selectedRow.crMigCL}</p>
+                                    <p className='bond-selected-credit-moodysRating'>{selectedRow.moodysRating}</p>
+                                    <p className='bond-selected-credit-crMigPred'>{selectedRow.crMigPred}</p>
+                                </div>
+                            </div>
+                            <div className='bond-selected-credit-spread'>
+                                <h2>Credit Spread Prediction</h2>
+                                <div className='bond-selected-credit-spread-stats'>
+                                    <p className='bond-selected-credit-percentage'>{selectedRow.crSpreadSL}</p>
+                                    <p className='bond-selected-credit-crSpreadPred'>{selectedRow.crSpreadPred}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </>)}
+                </div>
+            </div>
             {selectedCountry && (<h2>Selected Country: {selectedCountry.label}</h2>)}
             {selectedCompany && (<h2>Selected Company: {selectedCompany.label}</h2>)}
-            <h1 className='test'>BondOverview Page</h1>
         </div>
     )
 }
